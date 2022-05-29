7ea4d2c04ef6a50c0812bb681695397368b681a1
Mhema460 <105913037+Mhema460@users.noreply.github.com>
2022-05-29 22:57:58 +0530

    Update jenkins.tf

--- jenkins.tf
+++ jenkins.tf
@@ -4,7 +4,7 @@ provider "google" {
 }
 
 resource "google_compute_instance" "jenkins-master-1" {
-  name         = "jenkins-master-new1"
+  name         = "jenkins-master-1"
   machine_type = "n2-standard-4"
   zone         = "europe-west1-d"
 
