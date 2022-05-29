4a37b13b2a8e8a1cda3f41ac750af860c7becdd3
Mhema460 <105913037+Mhema460@users.noreply.github.com>
2022-05-30 00:01:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,17 @@
+provider "google" {
+  project  = "helical-song-350207"
+  region  = "europe-west1"
+}
 terraform {
   backend "gcs" {
     bucket = "hema-tf-state-prod"
     prefix = "terraform/state"
   }
+}
+module "kylo_ren" {
+  source           = "../DEV/**"
+  gcp_project_id   = helical-song-350207
+  vpc_network_name = "default"
+  instance_name    = "kylo-ren"
+  network_tags     = ["http-server", "https-server"]
 }
