4b67d1c96863a9af9daf5dee6efda8764b4f155a
Mhema460 <105913037+Mhema460@users.noreply.github.com>
2022-05-29 23:01:48 +0530

    Create slave.tf

---
+++ modules/jenkins-tf/slave.tf
@@ -0,0 +1,23 @@
+provider "google" {
+  project  = "helical-song-350207"
+  region  = "europe-west1"
+}
+
+resource "google_compute_instance" "jenkins-master-new1" {
+  name         = "jenkins-master-new1"
+  machine_type = "n2-standard-4"
+  zone         = "europe-west1-d"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-lts"
+    }
+  }
+
+  network_interface {
+      network = "default"
+      access_config {
+		 
+    }
+  }
+}
