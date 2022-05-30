3040ab3d49f1b2f7f5c6a35a359cde6f9e8595b4
Mhema460 <105913037+Mhema460@users.noreply.github.com>
2022-05-30 12:06:48 +0530

    Create jenkins.tf

---
+++ jenkins.tf
@@ -0,0 +1,21 @@
+resource "google_compute_instance" "jenkins-master-new1" {
+
+  name         = "jenkins-master-new1"
+  machine_type = "n2-standard-4"
+  zone         = "europe-west1-b"
+ 
+boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+
+ metadata_startup_script = "./script/master.sh"
+
+}
