b5648d39e43ad3ea7bced466d863ca6c255b7d66
Mhema460 <105913037+Mhema460@users.noreply.github.com>
2022-05-31 19:51:25 +0530

    Create seed.groovy

---
+++ groovy/seed.groovy
@@ -0,0 +1,21 @@
+categorizedJobsView('Hema - DEV') {
+    jobs {
+        regex('.*-flow|.*-tmpl|mip-.*(?<!(mapper|ear|sqls|logging|msba|properties|test))|Hema-mapper-utils|BAL')
+    }
+    categorizationCriteria {
+        regexGroupingRule(/Hema-.*(?<!(mapper|ear|sqls|logging|msba|properties|flow|test|tmpl))|Hema-mapper-utils/, 'DEV - Core')
+        regexGroupingRule(/^.*-flow/, 'DEV - Flows')
+        regexGroupingRule(/^.*-tmpl/, 'DEV - Templates')
+         
+         
+    }
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
+}
