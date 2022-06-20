928d32e271d4bc26e528d1db8949114c8c57828c
Mhema460 <105913037+Mhema460@users.noreply.github.com>
2022-06-20 12:53:14 +0000

    Update main.tf

--- Jenkins-tf/main.tf
+++ Jenkins-tf/main.tf
@@ -1,4 +1,30 @@
- resource "google_compute_instance" "hema" {
+terraform {
+  
+  required_providers {
+  google = {
+    source = "hashicorp/google"
+    version = "~>4.16.0"
+  }
+    
+  google-beta = {
+    source = "hashicorp/google-beta"
+    version = "~>3.0"
+   }
+  }
+}
+
+provider "google" {
+  project  = "helical-song-350207"
+  region  = "europe-west1"
+}
+terraform {
+  backend "gcs" {
+    bucket = "hema-tf-state-prod"
+    prefix = "terraform/state"
+  }
+}
+
+resource "google_compute_instance" "hema" {
 
   name         = "hema"
   machine_type = "e2-medium"
