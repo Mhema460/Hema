b3f2c07711588144202d71d2148b883b8cdcb267
Mhema460 <105913037+Mhema460@users.noreply.github.com>
2022-06-20 20:12:15 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
-resource "google_compute_instance" "slave-1" {
+resource "google_compute_instance" "master-slave-1" {
 
-  name         = "slave-1"
+  name         = "master-slave-1"
   machine_type = "e2-micro"
   zone         = "europe-west1-b"
   
