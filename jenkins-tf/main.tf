7f8e38920acc340ed1d0c0bd2b76205b7e359555
Mhema460 <105913037+Mhema460@users.noreply.github.com>
2022-06-24 11:57:51 +0000

    Create main.tf

---
+++ jenkins-tf/main.tf
@@ -0,0 +1,19 @@
+ resource "google_compute_instance" "test" {
+
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "europe-west1-b"
+  
+boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+
+ }
