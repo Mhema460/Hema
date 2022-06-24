69a7b3a323df4cb0128f85df97c544682a91d602
Mhema460 <105913037+Mhema460@users.noreply.github.com>
2022-06-24 14:10:29 +0530

    Create slave.sh

---
+++ jenkins-tf/slave.sh
@@ -0,0 +1,5 @@
+sudo apt update -y
+sudo apt install -y mc nano wget git curl
+#install java JDK 8 and 11
+sudo apt install openjdk-8-jre openjdk-8-jdk -y
+sudo apt install fontconfig openjdk-11-jdk -y
