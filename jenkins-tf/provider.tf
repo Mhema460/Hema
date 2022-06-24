87c43b09f775a9e971a0d17bd39af6d2e9ccbc45
Mhema460 <105913037+Mhema460@users.noreply.github.com>
2022-06-24 11:56:45 +0000

    Update provider.tf

--- jenkins-tf/provider.tf
+++ jenkins-tf/provider.tf
@@ -1,5 +1,25 @@
+terraform {
+  
+  required_providers {
+  google = {
+    source = "hashicorp/google"
+    version = "~>4.16.0"
+  }
+    
+  google-beta = {
+    source = "hashicorp/google-beta"
+    version = "~>3.0"
+   }
+  }
+}
+
 provider "google" {
-  project = "helical-song-350207"
+  project  = "helical-song-350207"
   region  = "europe-west1"
-  zone    = "europe-west1-b"
+}
+terraform {
+  backend "gcs" {
+    bucket = "hema-tf-state-prod"
+    prefix = "terraform/state"
+  }
 }
