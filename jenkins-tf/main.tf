b1a375a2c99843cddd9bfa2a4772ae3c780d6bbb
Mhema460 <105913037+Mhema460@users.noreply.github.com>
2022-06-29 20:30:33 +0530

    Update main.tf

--- jenkins-tf/main.tf
+++ jenkins-tf/main.tf
@@ -17,3 +17,22 @@ boot_disk {
   }
 
  }
+resource "google_compute_instance" "test1" {
+
+  name         = "test1"
+  machine_type = "e2-medium"
+  zone         = "europe-north1-b"
+  
+boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+
+ }
